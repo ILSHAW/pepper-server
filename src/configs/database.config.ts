4ef56b8c88b5490d4b211f218489aea9967f8f17
Andrew Neijhmak <pe4enka519@gmail.com>
2023-03-19 04:29:59 +0300

    SOME UPDATES

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,5 +1,4 @@
 export function factory() {
-    console.log(process.env.DATABASE_HOST, process.env.DATABASE_PORT, process.env.DATABASE_NAME)
     return {
         database: {
             host: process.env.DATABASE_HOST,
